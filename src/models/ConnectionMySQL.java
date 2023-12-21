3a10611ab688d4e80d2102df00dc634acba0f350
Juan David <juandariasgomez@gmail.com>
2023-12-20 20:49:03 -0500

    es hizo hasta el método listar empleados con la conexión a la base de datos

---
+++ src/models/ConnectionMySQL.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author juand
+ */
+public class ConnectionMySQL {
+    private String database_name = pharmacy_database;
+    private String user = "root";
+    private String password = "0514";
+    private String url = "jdbc:mysql://localhost:3306/" + database_name;
+    Connection conn = null;
+    
+    public Connection getConnection(){
+        try{
+            //Obtener valor del Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Obtener la conexión
+            conn = DriverManager.getConnection(url, user, password);
+        }catch(ClassNotFoundException e){
+            System.err.println("Ha ocurrido un ClassNotFoundException " + e.getMessage());
+        }catch(SQLException  e){
+            System.err.println("Ha ocurrido un SQLException " + e.getMessage());
+        }
+        return conn;
+    }
+}
---
+++ src/models/EmployeesDao.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.sql.Timestamp;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author juand
+ */
+public class EmployeesDao {
+
+    //Instanciar la conexión
+    ConnectionMySQL cn = new ConnectionMySQL();
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet rs;
+
+    //Variables para enviar datos entre interfaces
+    public static int id_user = 0;
+    public static String full_name_user = "";
+    public static String username_user = "";
+    public static String address_user = "";
+    public static String telephone_user = "";
+    public static String email_user = "";
+    public static String rol_user = "";
+
+    //Método de login
+    public Employees loginQuery(String user, String password) {
+        String query = "SELECT * FROM employees WHERE username = ? AND password = ?";
+        Employees employee = new Employees();
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            //Enviar parámetros
+            pst.setString(1, user);
+            pst.setString(2, password);
+            rs = pst.executeQuery();
+
+            if (rs.next()) {
+                employee.setId(rs.getInt("id"));
+                id_user = employee.getId();
+                employee.setFull_name(rs.getString("full_name"));
+                full_name_user = employee.getFull_name();
+                employee.setUsername(rs.getString("username"));
+                username_user = employee.getUsername();
+                employee.setAddress(rs.getString("address"));
+                address_user = employee.getAddress();
+                employee.setTelephone(rs.getString("telephone"));
+                telephone_user = employee.getTelephone();
+                employee.setEmail(rs.getString("email"));
+                email_user = employee.getEmail();
+                employee.setRol(rs.getString("rol"));
+                rol_user = employee.getRol();
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al obtener al empleado" + e);
+        }
+        return employee;
+    }
+
+    //Registrar empleado
+    public boolean registerEmployeeQuery(Employees employee) {
+        String query = "INSERT INTO employees(id, full_name, username, address, telephone, email, password, rol, created"
+                + "update) VALUES(?,?,?,?,?,?,?,?,?,?)";
+
+        Timestamp datetime = new Timestamp(new Date().getTime());
+
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setInt(1, employee.getId());
+            pst.setString(2, employee.getFull_name());
+            pst.setString(3, employee.getUsername());
+            pst.setString(4, employee.getAddress());
+            pst.setString(5, employee.getTelephone());
+            pst.setString(6, employee.getEmail());
+            pst.setString(7, employee.getPassword());
+            pst.setString(8, employee.getRol());
+            pst.setTimestamp(9, datetime);
+            pst.setTimestamp(10, datetime);
+            return true;
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al registrar al empleado" + e);
+            return false;
+        }
+    }
+
+    //Listar empleado
+    public List listEmployeesQuery(String value) {
+        List<Employees> list_employees = new ArrayList();
+        String query = "SELECT * FROM employees ORDER BY rol ASC";
+        String query_search_employee = "SELECT * FROM employees WHERE id LIKE '%" + value + "%'";
+        try {
+            conn = cn.getConnection();
+            if (value.equalsIgnoreCase("")) {
+                pst = conn.prepareStatement(query);
+                rs = pst.executeQuery();
+            } else {
+                pst = conn.prepareStatement(query_search_employee);
+                rs = pst.executeQuery();
+
+            }
+
+            while (rs.next()) {
+                Employees employee = new Employees();
+                employee.setId(rs.getInt("id"));
+                employee.setFull_name(rs.getString("full_name"));
+                employee.setUsername(rs.getString("username"));
+                employee.setAddress(rs.getString("address"));
+                employee.setTelephone(rs.getString("telephone"));
+                employee.setEmail(rs.getString("email"));
+                employee.setRol(rs.getString("rol"));
+                list_employees.add(employee);
+
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+        return list_employees;
+    }
+}
