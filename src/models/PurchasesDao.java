c75747757ff5c42123f74df582ef13808f84fd14
Juan David <juandariasgomez@gmail.com>
2023-12-22 22:08:48 -0500

    métodos del modelo productos y metodos sin el Dao de ventas

---
+++ src/models/PurchasesDao.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.sql.Timestamp;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author juand
+ */
+public class PurchasesDao {
+    //Instanciar la conexión
+    ConnectionMySQL cn = new ConnectionMySQL();
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet rs;
+}
